c7f3362fe30be9330a118df02f753689d9d55690
al Mohamady <al.Mohamady47@gmail.com>
2020-05-25 16:47:53 +0200

    Add project files.

---
+++ SOLID_PROJECT/IUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PROJECT
+{
+    interface IUser
+    {
+        bool Login(string userName, string password);
+        bool Register(string userName, string password, string emil);
+    }
+
+    interface ILogger
+    {
+        void LogError(string error);
+    }
+
+    interface IEmail
+    {
+        bool SendEmail(string emailContent);
+    }
+}
