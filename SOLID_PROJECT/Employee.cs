cc254628bf29a28f93aeee31788c270339dbb58f
al Mohamady <al.Mohamady47@gmail.com>
2020-05-25 18:43:22 +0200

    Open Close Principle

---
+++ SOLID_PROJECT/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PROJECT
+{
+    public abstract class Employee
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+
+        public Employee()
+        { }
+
+        public Employee(int id , string name )
+        {
+            this.ID = id;
+            this.name = name;
+        }
+
+        public abstract decimal CalculateBonus(decimal salary);
+
+        public override string ToString()
+        {
+            return string.Format("ID: {0}, Name: {1}", this.ID, this.name);
+        }
+    }
+
+    public class PermanentEmployee : Employee
+    {
+        public PermanentEmployee(int id, string name) : base(id, name)
+        { 
+        
+        }
+
+        public override decimal CalculateBonus(decimal salary)
+        {
+            return salary * 0.1M;
+        }
+    }
+
+    public class TempEmployee : Employee
+    {
+        public TempEmployee(int id, string name) : base(id, name)
+        {
+
+        }
+
+        public override decimal CalculateBonus(decimal salary)
+        {
+            return salary * 0.05M;
+        }
+    }
+
+
+}
--- SOLID_PROJECT/Program.cs
+++ SOLID_PROJECT/Program.cs
@@ -10,6 +10,20 @@ namespace SOLID_PROJECT
     {
         static void Main(string[] args)
         {
+
+            Employee employee = new PermanentEmployee(1, "Ahmed");
+
+            Console.WriteLine("Emplyee: {0}  Bouns: {1} ", employee.ToString()
+                , employee.CalculateBonus(100000).ToString());
+
+
+            Employee employee2 = new TempEmployee(1, "Mohamed");
+
+            Console.WriteLine("Emplyee: {0}  Bouns: {1} ", employee2.ToString()
+                , employee2.CalculateBonus(100000).ToString());
+
+            Console.ReadKey(true);
+
         }
     }
 }
